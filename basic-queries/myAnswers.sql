233511f67a32d9d421c1dd90662d6222b14388bc
raspi <gitraspigner@gmail.com>
2025-11-17 14:28:56 -0800

    add responses to questions 3-6

--- basic-queries/myAnswers.sql
+++ basic-queries/myAnswers.sql
@@ -8,13 +8,13 @@
 use northwind;
 select productid, productname, unitprice from products;
 # 3. Write a query to list the product id, product name, and unit price of every product. Except this time, order them in ascending order by price.
-
+select productid, productname, unitprice from products order by unitprice asc;
 # 4. What are the products that we carry where the unit price is $7.50 or less?
-
+select productid, productname, unitprice from products where unitprice <= 7.50;
 # 5. What are the products that we carry where we have at least 100 units on hand? Order them in descending order by price.
-
+select productid, productname, unitprice, unitsinstock from products where unitsinstock >= 100 order by unitprice desc;
 # 6. What are the products that we carry where we have at least 100 units on hand? Order them in descending order by price. If two or more have the same price, list those in ascending order by product name.
-
+select productid, productname, unitprice, unitsinstock from products where unitsinstock >= 100 order by unitprice desc, productname asc;
 # 7. What are the products that we carry where we have no units on hand, but 1 or more units of them on backorder? Order them by product name.
 
 # 8. What is the name of the table that holds the types (categories) of the items Northwind sells?
