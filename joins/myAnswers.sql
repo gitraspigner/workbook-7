e7e6aac0f4174c135107f0ac2bdbfefe1d4d1674
raspi <gitraspigner@gmail.com>
2025-11-18 10:25:12 -0800

    add some responses but not all

--- joins/myAnswers.sql
+++ joins/myAnswers.sql
@@ -4,13 +4,13 @@
 # 1. List the product id, product name, unit price and category name of all
 # products. Order by category name and within that, by product name.
 use northwind;
-
+SELECT p.ProductID, p.ProductName, p.UnitPrice, c.CategoryName FROM products p JOIN categories c ON p.CategoryID = c.CategoryID;
 # 2. List the product id, product name, unit price and supplier name of all
 # products that cost more than $75. Order by product name.
-
+SELECT p.ProductID, p.ProductName, p.UnitPrice, s.CompanyName FROM products p JOIN suppliers s ON p.SupplierID = s.SupplierID WHERE p.UnitPrice > 75 ORDER BY p.ProductName ASC;
 # 3. List the product id, product name, unit price, category name, and supplier
 # name of every product. Order by product name.
-
+#  ---LEFT OFF HERE ---- SELECT p.ProductID, p.ProductName, p.UnitPrice, 
 # 4. What is the product name(s) and categories of the most expensive
 # products? HINT: Find the max price in a subquery and then use that in
 # your more complex query that joins products with categories.
