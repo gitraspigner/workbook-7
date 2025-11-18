c25aea6094dc40820f387f49222b214bbfa38de7
raspi <gitraspigner@gmail.com>
2025-11-18 13:06:02 -0800

    add start file for exercise

---
+++ insert-update-delete/myAnswers.sql
@@ -0,0 +1,21 @@
+# Ravi Spigner
+# 11/18/2025
+# Workbook 7-Exercise 6: Insert-Update-Delete -Northwind Traders
+# 1. Add a new supplier.
+use northwind;
+
+# 2. Add a new product provided by that supplier
+
+# 3. List all products and their suppliers.
+
+# 4. Raise the price of your new product by 15%.
+
+# 5. List the products and prices of all products from that supplier.
+
+# 6. Delete the new product.
+
+# 7. Delete the new supplier.
+
+# 8. List all products.
+
+# 9. List all suppliers.
