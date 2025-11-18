d96f6ba470459bac01bb73a427876233b9ffb8b4
raspi <gitraspigner@gmail.com>
2025-11-18 10:03:58 -0800

    create answers sql file/directory for my responses

---
+++ joins/myAnswers.sql
@@ -0,0 +1,22 @@
+# Ravi Spigner
+# 11/18/2025
+# Workbook 7-Exercise 4-Northwind Traders
+# 1. List the product id, product name, unit price and category name of all
+# products. Order by category name and within that, by product name.
+use northwind;
+
+# 2. List the product id, product name, unit price and supplier name of all
+# products that cost more than $75. Order by product name.
+
+# 3. List the product id, product name, unit price, category name, and supplier
+# name of every product. Order by product name.
+
+# 4. What is the product name(s) and categories of the most expensive
+# products? HINT: Find the max price in a subquery and then use that in
+# your more complex query that joins products with categories.
+
+# 5. List the order id, ship name, ship address, and shipping company name of
+# every order that shipped to Germany.
+
+# 6. List the order id, order date, ship name, ship address of all orders that
+# ordered "Sasquatch Ale"?
