9f5caea00399ba07c64a46a8adbdfa9991ed3f0b
raspi <gitraspigner@gmail.com>
2025-11-18 09:55:07 -0800

    remove blank lines

--- basic-queries/myAnswers.sql
+++ basic-queries/myAnswers.sql
@@ -1,7 +1,6 @@
 # Ravi Spigner
 # 11/17/2025
 # Workbook 7-Exercise 2-Northwind Traders
-
 # 1. What is the name of the table that holds the items Northwind sells?
 # products
 # 2. Write a query to list the product id, product name, and unit price of every product.
