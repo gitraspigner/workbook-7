e5561b6ee61e659263acbbc9e2a4d990faec26a3
raspi <gitraspigner@gmail.com>
2025-11-18 12:09:41 -0800

    add rest of responses

--- joins/myAnswers.sql
+++ joins/myAnswers.sql
@@ -14,9 +14,11 @@ SELECT p.ProductID, p.ProductName, p.UnitPrice, c.CategoryName, s.CompanyName FR
 # 4. What is the product name(s) and categories of the most expensive
 # products? HINT: Find the max price in a subquery and then use that in
 # your more complex query that joins products with categories.
-
+# This is to say, what is the most expensive product out of any category and what is its category name (and I'm inlcuding the price).
+SELECT p.ProductName, p.UnitPrice, c.CategoryName FROM products p JOIN categories c ON p.CategoryID = c.CategoryID WHERE p.UnitPrice >= (SELECT MAX(UnitPrice) FROM products);
 # 5. List the order id, ship name, ship address, and shipping company name of
 # every order that shipped to Germany.
-
+SELECT o.OrderID, o.ShipName, o.ShipAddress, s.CompanyName FROM orders o JOIN shippers s ON o.ShipVia = s.ShipperID WHERE o.ShipCountry = 'Germany';
 # 6. List the order id, order date, ship name, ship address of all orders that
 # ordered "Sasquatch Ale"?
+SELECT o.OrderID, o.ShippedDate, o.ShipName, o.ShipAddress FROM orders o JOIN `order details` od ON o.OrderID = od.OrderID WHERE od.ProductID = (SELECT p.ProductID FROM products p WHERE p.ProductName = 'Sasquatch Ale');
