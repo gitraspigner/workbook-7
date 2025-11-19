32a7f6012d1f24cbcf3c3ac80c1a107b16d15f98
raspi <gitraspigner@gmail.com>
2025-11-19 10:53:19 -0800

    add responses to questions

--- insert-update-delete/myAnswers.sql
+++ insert-update-delete/myAnswers.sql
@@ -10,17 +10,23 @@ SELECT * FROM Suppliers;
 INSERT INTO Products (ProductID, ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued)
 VALUES(NULL, 'Cheese', 30, 4, '500g', 10.50, 47, 0, 40, 0); # With first arg as null (which is ProductID, use next available entry which is 78)
 # Test
-SELECT * FROM Products WHERE SupplierID = (SELECT SupplierID FROM Suppliers WHERE CompanyName = 'The Ravi Company');
+#SELECT * FROM Products WHERE SupplierID = (SELECT SupplierID FROM Suppliers WHERE CompanyName = 'The Ravi Company');
 # 3. List all products and their suppliers.
 SELECT s.CompanyName, p.*  FROM Products p JOIN Suppliers s ON p.SupplierID = s.SupplierID ORDER BY p.SupplierID DESC;
 # 4. Raise the price of your new product by 15%.
 UPDATE Products SET UnitPrice = UnitPrice * 1.15 WHERE ProductName = 'Cheese';
 # 5. List the products and prices of all products from that supplier.
-
+SELECT * FROM Products WHERE SupplierID = (SELECT SupplierID FROM Suppliers WHERE CompanyName = 'The Ravi Company');
 # 6. Delete the new product.
-
+DELETE FROM Products WHERE ProductName = 'Cheese';
+# Tests
+#SELECT * FROM Products WHERE SupplierID = (SELECT SupplierID FROM Suppliers WHERE CompanyName = 'The Ravi Company');
+#SELECT * FROM Products WHERE ProductName = 'Cheese';
 # 7. Delete the new supplier.
-
+DELETE FROM Suppliers WHERE CompanyName = 'The Ravi Company';
+# Test
+#SELECT * FROM Suppliers WHERE CompanyName = 'The Ravi Company';
 # 8. List all products.
-
+SELECT * FROM Products;
 # 9. List all suppliers.
+SELECT * FROM Suppliers;
